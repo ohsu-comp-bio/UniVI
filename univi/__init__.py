0970f622e37d392279bef80ef91653581a6641bd
Andrew Ashford <ashforda@ohsu.edu>
2025-12-15 14:14:31 -0800

    Bump version to v0.2.9

--- univi/__init__.py
+++ univi/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import Any, List
 
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 
 # Eager (fast/light) public API
 from .config import ModalityConfig, UniVIConfig, TrainingConfig
